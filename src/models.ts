1782108f4e1b23aaf1cb753cd919e662cc9107f7
Andrey Malyshkin <andrishkin@gmailc,om>
2025-04-05 00:37:49 +0300

    fix:fix

--- src/models.ts
+++ src/models.ts
@@ -13,13 +13,16 @@ export const getDynamicModel = (tableName: string) => {
     if (mongoose.models[tableName]) {
         return mongoose.models[tableName];
     }
-    
+
     return mongoose.model(tableName, new mongoose.Schema({
         id: { type: String, required: true },
         name: { type: String, required: true },
         price: { type: Number, required: true },
         link: { type: String, required: false },
-        count: {type: Number, required: false},
-        nickName: {type: String, required: true}
+        count: { type: Number, required: false },
+        nickName: { type: String, required: true }
+    }, {
+        strict: true,
+        versionKey: false
     }));
 };
